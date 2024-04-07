cb38d10195afeca1a9a4b3dcaacc68083ad3bdfa
dhruv0020 <163663023+dhruv0020@users.noreply.github.com>
2024-04-08 00:27:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Dropdown from './components/Dropdown';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Dynamic Tables</h1>
+      <Dropdown />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,15 @@
+// src/data.js
+export const table1Data = [
+    { id: 1, name: 'Ramesh', age: 25 },
+    { id: 2, name: 'Gurkerth', age: 30 },
+  ];
+
+  export const table2Data = [
+    { id: 101, product: 'Blender', price: 10.99, rating : 9 },
+    { id: 102, product: 'Iron', price: 15.49, rating : 5},
+  ];
+  
+  export const table3Data = [
+    { id: 'a_1', category: 'Apple', quantity: 50 },
+    { id: 'b_2', category: 'Banana', quantity: 30 },
+  ];
